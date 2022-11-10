e96f4c9d1e00323b8404381b07131718a3feba54
Mahipat Parmar <memahipat303@gmail.com>
2022-11-10 23:33:56 +0530

    bin class and admin Dao added

---
+++ src/course_monitor/utility/BatchException.java
@@ -0,0 +1,12 @@
+package course_monitor.utility;
+
+public class BatchException extends Exception {
+
+	public BatchException() {
+
+	}
+
+	public BatchException(String message) {
+		super(message);
+	}
+}
---
+++ src/course_monitor/utility/FacultyException.java
@@ -0,0 +1,12 @@
+package course_monitor.utility;
+
+public class FacultyException extends Exception {
+
+	public FacultyException() {
+
+	}
+
+	public FacultyException(String message) {
+		super(message);
+	}
+}
